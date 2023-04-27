cf64fceaec01448373e2b575f8a03a8b33cf182d
maskara10 <vinivini147147147@gmail.com>
2023-04-27 16:44:51 -0300

    Adicionando logica a aplicação

--- script.js
+++ script.js
@@ -1,65 +1,98 @@
-// // URL da API que você deseja consumir
-// const apiUrl = "https://makeup-api.herokuapp.com/api/v1/products.json";
-
-// // Fazer a requisição usando o fetch
-// fetch(apiUrl)
-//   .then((response) => {
-//     // Verificar se a resposta da API está OK
-//     if (!response.ok) {
-//       throw new Error(
-//         `Erro ao fazer a requisição. Código de status: ${response.status}`
-//       );
-//     }
-//     // Converter a resposta para JSON
-//     return response.json();
-//   })
-//   .then((data) => {
-//     //var listarProdutos = document.querySelector(".body")
-
-//     // Manipular os dados retornados pela API
-//     console.log(data);
-//     var marcas = data.filter((item) => item.category == "pencil");
-//     //var tipo = marcas.filter((item) => item.product_type == "eyeliner")
-//     //console.log(marcas)
-//     //console.log(tipo)
-//     //var preco = tipo[2].price
-//     //console.log(preco * 5.5)
-//     marcas.sort((i1, i2) => {
-//       if (i1.name > i2.name) {
-//         return -1;
-//       } else if (i1.name < i2.name) {
-//         return 1;
-//       } else {
-//         return 0;
-//       }
-//     });
-//     console.log(data[0].name);
-//     //console.log(data)
-//     //console.log(data[0].price * 5.5)
-//   })
-//   .catch((error) => {
-//     // Lidar com erros
-//     console.error(error);
-//   });
 
 let dados;
+var contadorMostrarMais = 10;
+var main = document.querySelector(".main");
 
 fetch("https://makeup-api.herokuapp.com/api/v1/products.json")
   .then((response) => response.json())
   .then((json) => {
     dados = json;
     console.log(dados);
-  });
-
-var main = document.querySelector(".main");
-function listarP() {
-  for (let index = 0; index < 20; index++) {
-    main.innerHTML += `
+    for (let index = 0; index < contadorMostrarMais; index++) {
+      main.innerHTML += `
       <div class="product">
           <img class="img_product" src="${dados[index].image_link}" alt="exemplo">
           <p class="name_product">${dados[index].name}</p>
           <a class="marca_product">${dados[index].brand}</a><a class="price_product" >R$ ${dados[index].price * 5.5}</a>
       </div>
       `;
+    }
+  });
+
+function buscaNome() {
+  main.innerHTML = ""
+  var inputText = document.querySelector(".buscar_nome").value
+  var buscar_nome = dados.filter((item) => item.name.toLowerCase() == inputText.toLowerCase())
+  console.log(buscar_nome)
+  for (let index = 0; index < buscar_nome.length; index++) {
+    main.innerHTML += `
+      <div class="product">
+          <img class="img_product" src="${buscar_nome[index].image_link}" alt="exemplo">
+          <p class="name_product">${buscar_nome[index].name}</p>
+          <a class="marca_product">${buscar_nome[index].brand}</a><a class="price_product" >R$ ${buscar_nome[index].price * 5.5}</a>
+      </div>
+      `;
   }
 }
+
+var contadorMostarMenos = 10
+function mostrarMais() {
+
+  contadorMostrarMais = contadorMostrarMais + 10
+  for (contadorMostarMenos; contadorMostarMenos < contadorMostrarMais; contadorMostarMenos++) {
+    main.innerHTML += `
+    <div class="product">
+        <img class="img_product" src="${dados[contadorMostarMenos].image_link}" alt="exemplo">
+        <p class="name_product">${dados[contadorMostarMenos].name}</p>
+        <a class="marca_product">${dados[contadorMostarMenos].brand}</a><a class="price_product" >R$ ${dados[contadorMostarMenos].price * 5.5}</a>
+    </div>
+    `;
+  }
+}
+
+function selectItens() {
+  var select = document.getElementById("rating")
+  if (select.value == "a-z") {
+    dados.sort((i1, i2) => {
+      if (i1.name < i2.name) {
+        return -1;
+      } else if (i1.name > i2.name) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+    main.innerHTML = ""
+    for (let index = 0; index < contadorMostrarMais; index++) {
+      main.innerHTML += `
+      <div class="product">
+          <img class="img_product" src="${dados[index].image_link}" alt="exemplo">
+          <p class="name_product">${dados[index].name}</p>
+          <a class="marca_product">${dados[index].brand}</a><a class="price_product" >R$ ${dados[index].price * 5.5}</a>
+      </div>
+      `;
+    }
+  } else if(select.value == "maiores-precos") {
+
+    dados.sort((i1, i2) => {
+      if (i1.price > i2.price) {
+        return -1;
+      } else if (i1.price < i2.price) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+    main.innerHTML = ""
+    for (let index = 0; index < contadorMostrarMais; index++) {
+      main.innerHTML += `
+      <div class="product">
+          <img class="img_product" src="${dados[index].image_link}" alt="exemplo">
+          <p class="name_product">${dados[index].name}</p>
+          <a class="marca_product">${dados[index].brand}</a><a class="price_product" >R$ ${dados[index].price * 5.5}</a>
+      </div>
+      `;
+    }
+  }
+}
+
